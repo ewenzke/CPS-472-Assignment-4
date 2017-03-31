88801c85a20f1519f3c06029578c7a9d54530860
ewenzke <wenzkee1@udayton.edu>
2017-03-31 19:51:21 -0400

    Update MD5ConstantCheck.py

--- MD5ConstantCheck.py
+++ MD5ConstantCheck.py
@@ -1,7 +1,7 @@
 MD5Values = [ '"d7 6a a4 78"', '"e8 c7 b7 56"', '"24 20 70 db"', '"c1 bd ce ee"' ]
 min,max = MinEA(),MaxEA()
 MD5Present = False
-print "The harder part of this problem appears to be getting FindBinary to accept a stinking input"
+print "Checking for MD5 constants..."
 
 for MD5 in MD5Values:
 	addr = FindBinary(min,SEARCH_DOWN,MD5,16)
